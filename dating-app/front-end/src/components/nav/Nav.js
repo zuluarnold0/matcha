c342e6b595a0a4040fbc0519e45c2ad3ba9778ba
zuluarnold0 <zuluarnold0@gmail.com>
2020-06-03 04:17:24 +0200

    finalised filter and nav changes

--- dating-app/front-end/src/components/nav/Nav.js
+++ dating-app/front-end/src/components/nav/Nav.js
@@ -6,9 +6,12 @@ const Nav = () => {
     return (
         <nav className="nav-ba">
             <ul>
+                <div className="push-nav-start">
                 <li>
                     <NavLink className="logo-matcha" to="/">Matcha</NavLink>
                 </li>
+                </div>
+                <div className="push-nav-end">
                 <li>
                     <NavLink to="/profile" className="nav-img">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm2hIJK-htqNGFQUUtshHh934Z_J3CDlSe9H7UHLWln9by7CoS" alt="img"/>
@@ -27,6 +30,7 @@ const Nav = () => {
                 <li>
                     <Link className="nav-logout nav-tags fas fa-power-off" to="/signin"></Link>
                 </li>
+                </div>
             </ul>
         </nav>
     )
