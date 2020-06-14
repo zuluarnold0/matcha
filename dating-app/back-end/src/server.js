d1e9a3d8251585bd3343f7a2a8f3701d9150a2ec
zuluarnold0 <zuluarnold0@gmail.com>
2020-06-15 01:34:34 +0200

    created database matcha

--- dating-app/back-end/src/server.js
+++ dating-app/back-end/src/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+//const bodyParser = require('body-parser');
+//const bcrypt = require('bcrypt-nodejs');
+//const cors = require('cors');
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : '',
+        password : 'rootadmin',
+        database : 'matcha'
+    }
+});
+
+console.log(knex.select('*').from('users'));
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`matcha app is listening at http://localhost:${port}`));
