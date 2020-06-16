076d6c3818943dea03d9d1d7e2a025c6932b1e23
zuluarnold0 <zuluarnold0@gmail.com>
2020-06-16 09:49:07 +0200

    register route

--- dating-app/back-end/src/server.js
+++ dating-app/back-end/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 //const bcrypt = require('bcrypt-nodejs');
 //const cors = require('cors');
 
-const knex = require('knex')({
+const db = require('knex')({
     client: 'pg',
     connection: {
         host : '127.0.0.1',
@@ -13,11 +13,39 @@ const knex = require('knex')({
     }
 });
 
-console.log(knex.select('*').from('users'));
-
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/register', function (req, res) {
+    const { firstname, lastname, username, email, bio, gender, age, sexPref, tags } = req.body;
+    fetch('https://ipapi.co/json')
+    .then(res => res.json())
+    .then(loc => {
+        db('users')
+        .returning('*')
+        .insert({ 
+            firstname: firstname,
+            lastname: lastname,
+            username: username,
+            email: email,
+            bio: bio,
+            gender: gender,
+            age: age,
+            sexPref: sexPref,
+            tags: tags,
+            city: loc.city,
+            longi: loc.longitude,
+            lati: loc.latitude,
+            popularity: 0,
+            logged_time: new Date(),
+            is_logged_in: false,
+        })
+        .then(user => {
+            res.json(user[0]) 
+        })
+        .catch(err => res.status(400).json('Unable to register'));
+    })
+    .catch(err => res.status(400).json('location not found'));
+})
 
 app.listen(port, () => console.log(`matcha app is listening at http://localhost:${port}`));
